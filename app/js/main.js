b46f882f9e95137366704037a013bfcad20ffdde
Sovel <nikita.elagin2012@yandex.ru>
2021-06-03 12:03:59 +0300

    urpade

---
+++ app/js/main.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".hero__slider .swiper-container", {
+  spaceBetween: 30,
+  effect: "fade",
+  autoplay: {
+    delay: 1000,
+  },
+  loop:"true"
+  // navigation: {
+  //   nextEl: ".swiper-button-next",
+  //   prevEl: ".swiper-button-prev",
+  // },
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+});
